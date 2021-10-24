511da05472ce24763c96eacf8a13c8a543c26d62
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2021-10-25 01:45:59 +0300

    Update Fast-cleaner.sh

--- Fast-cleaner.sh
+++ Fast-cleaner.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 #  Name scipt: FastCleaner 
-#  Release date: 2021-07-12 
-#  Version: 1.0
+#  Release date: 2021-10-20 
+#  Version: 1.6
 #  Copyright (C) Georgii Bogdanov | GBOG@protonmail.com
-
-PASSWORD=$(zenity --password --title "The Script will now install the requiered dependencies, enter your password to proceed")
 
 set -eu
 
+snap refresh
+
 LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
-        snap remove "$snapname" --revision="$revision"
+        sudo snap remove "$snapname" --revision="$revision"
     done
-flatpak remove --unused
-rm /var/lib/snapd/cache/*
-eopkg dc
-eopkg rmo
-rm -f ~/.cache/thumbnails/*
-journalctl --vacuum-time=2day
+sudo flatpak update
+sudo flatpak remove --unused
+sudo rm /var/lib/snapd/cache/*
+sudo eopkg dc
+sudo eopkg rmo
+sudo journalctl --vacuum-size=64M
+sudo journalctl --vacuum-time=1days
+sudo rm -r ~/.cache/thumbnails/*
+sudo usysconf run -f
